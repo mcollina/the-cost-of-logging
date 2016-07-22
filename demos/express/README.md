297ee40a88a17927f7f99b5136c09f0bab592e34
Matteo Collina <hello@matteocollina.com>
2016-07-22 10:02:26 +0200

    Added express-pino demo.

---
+++ demos/express/README.md
@@ -0,0 +1,8 @@
+# Demos
+
+1. show "express-server.js"
+2. run `./bench.sh express-server.js`
+1. show "bunyan-server.js"
+2. run `./bench.sh buyan-server.js`
+1. show "pino-server.js"
+2. run `./bench.sh pino-server.js`
---
+++ demos/express/bench.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+if [[ ! -e $1 ]]; then
+  echo "script not found"
+  exit 1
+fi
+
+echo "> node $1 &"
+
+node $1 &> /dev/null &
+
+PID=$!
+
+CMD="autocannon -c 100 -p 10 localhost:3000"
+
+echo "> $CMD"
+
+$CMD
+
+
+kill -9 $PID
